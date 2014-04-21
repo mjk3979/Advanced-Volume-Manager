51364ecdbec7d54a03563ec6951adeae9ccb67d1
Matthew Koontz <mjk3979@rit.edu>
2014-04-21 13:53:36 -0400

    Added initial project files.

---
+++ src/com/matthewkoontz/advvolmngr/AdvancedVolumeManager.java
@@ -0,0 +1,15 @@
+package com.matthewkoontz.advvolmngr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AdvancedVolumeManager extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
